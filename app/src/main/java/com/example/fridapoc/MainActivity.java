7e42084e7a0c25ce45c141aa4f5f910489beffac
Raghu Saxena <poiasdpoiasd@live.com>
2023-10-13 14:14:34 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/fridapoc/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.fridapoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.fridapoc.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fridapoc' library on application startup.
+    static {
+        System.loadLibrary("fridapoc");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'fridapoc' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
